73aae1fdb24c602dbaa6d43ced553c6319d1af1f
Aaron193 <76023256+Aaron193@users.noreply.github.com>
2025-08-12 15:14:53 -0400

    we don't need this code here, it'll be on livewire

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -9,32 +9,8 @@ use Illuminate\Http\Request;
 class MemberController extends Controller
 {
     public function index(Request $request)
-    {
-        $query = Entity::where('entity_type', 'person');
-        
-        // Search functionality
-        if ($request->has('search') && $request->search) {
-            $search = $request->search;
-            $query->where(function($q) use ($search) {
-                $q->where('name', 'like', "%{$search}%")
-                  ->orWhere('first_name', 'like', "%{$search}%")
-                  ->orWhere('last_name', 'like', "%{$search}%")
-                  ->orWhere('company', 'like', "%{$search}%")
-                  ->orWhere('email', 'like', "%{$search}%");
-            });
-        }
-        
-        // Tag filtering
-        if ($request->has('tag') && $request->tag) {
-            $query->whereHas('tags', function($q) use ($request) {
-                $q->where('tags.id', $request->tag);
-            });
-        }
-        
-        $members = $query->with(['tags', 'tags.category'])->paginate(20);
-        $tagCategories = TagCategory::with('tags')->get();
-        
-        return view('members.index', compact('members', 'tagCategories'));
+    {        
+        return view('members.index');
     }
     
     public function show(Entity $member)
