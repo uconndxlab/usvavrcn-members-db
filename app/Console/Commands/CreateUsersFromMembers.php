e6a843a79cdb949708210a3d5c9666b0505daa01
Aaron193 <76023256+Aaron193@users.noreply.github.com>
2025-08-13 17:50:38 -0400

    console command to create user accounts from members

---
+++ app/Console/Commands/CreateUsersFromMembers.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Entity;
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class CreateUsersFromMembers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'users:create-from-members';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a User account for whichever members don\'t have one';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $members = Entity::where('entity_type', 'person')
+            ->whereNotNull('email')
+            ->get(['email', 'name']);
+
+        $accountsCreated = 0;
+
+        foreach ($members as $member) {
+
+            $userExists = User::where('email', $member->email)->exists();
+
+            $user = User::firstOrCreate([
+                'email' => $member->email,
+            ], [
+                'name' => $member->name,
+                'password' => Hash::make(Str::random(16)), // This line is very slow (Hash::make)
+            ]);
+
+            if (!$userExists) {
+                $this->info("Created user: {$user->email}");
+                $accountsCreated++;
+            }
+        }
+
+        $this->info("{$accountsCreated} accounts created");
+    }
+}
